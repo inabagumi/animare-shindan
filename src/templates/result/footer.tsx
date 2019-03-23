95ff6305e624ea3ca9398910cdc2041a9a58ba09
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-03-24 01:05:41 +0900

    feat: change copyright (#5)

--- src/templates/result/footer.tsx
+++ src/templates/result/footer.tsx
@@ -1,7 +1,8 @@
-import * as React from 'react'
-import { default as styled } from 'styled-components'
+import React, { FunctionComponent, ReactElement } from 'react'
+import styled from 'styled-components'
+import Copyright from '../../components/copyright'
 
-const FooterWrapper = styled.footer`
+const Base = styled.footer`
   background-color: #212121;
   margin: 56px -20px 0;
 
@@ -11,7 +12,7 @@ const FooterWrapper = styled.footer`
   }
 `
 
-const Footer = styled.div`
+const Content = styled.div`
   margin: 0 auto;
   max-width: 680px;
   padding: 36px 20px 32px;
@@ -21,32 +22,12 @@ const Footer = styled.div`
   }
 `
 
-const Copyright = styled.p`
-  color: #595959;
-  font-size: 1.3rem;
-  margin: 28px 0 0;
-  letter-spacing: 0.04rem;
-  text-align: center;
-
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-
-  @media (min-width: 500px) {
-    font-size: 1.5rem;
-  }
-`
-
-export default () => (
-  <FooterWrapper>
-    <Footer>
-      <Copyright>
-        Copyright 2018{' '}
-        <a href="https://ykzts.com/" rel="noopener noreferrer" target="_blank">
-          山岸 和利
-        </a>
-      </Copyright>
-    </Footer>
-  </FooterWrapper>
+const Footer: FunctionComponent = (): ReactElement => (
+  <Base>
+    <Content>
+      <Copyright invert />
+    </Content>
+  </Base>
 )
+
+export default Footer
