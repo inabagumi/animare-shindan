04bcaa0263985f0169e5b22d24e093c760f6ab3a
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-03-24 03:02:52 +0900

    use useStaticQuery

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,20 +1,9 @@
-import { StaticQuery, graphql } from 'gatsby'
-import * as React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import React, { FunctionComponent, ReactElement } from 'react'
 import { Helmet } from 'react-helmet'
-import { default as mainVisual } from '../images/main-visual.jpg'
+import mainVisual from '../images/main-visual.jpg'
 
-const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        defaultDescription: description
-        siteUrl
-      }
-    }
-  }
-`
-
-interface Props {
+type Props = {
   description?: string
   image?: {
     publicURL: string
@@ -22,58 +11,59 @@ interface Props {
   pathname: string
 }
 
-export default (({ description, image, pathname }) => (
-  <StaticQuery
-    query={query}
-    render={({ site }) => {
-      const { defaultDescription, siteUrl } = site.siteMetadata
+const SEO: FunctionComponent<Props> = ({
+  description,
+  image,
+  pathname
+}): ReactElement => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            defaultDescription: description
+            siteUrl
+          }
+        }
+      }
+    `
+  )
+  const { defaultDescription, siteUrl } = site.siteMetadata
 
-      return (
-        <Helmet>
-          <link href={`${new URL(pathname, siteUrl)}`} rel="canonical" />
-          <meta
-            content={description || defaultDescription}
-            name="description"
-          />
-          <meta content="website" property="og:type" />
-          <meta content={`${new URL(pathname, siteUrl)}`} property="og:url" />
-          <meta
-            content={`${new URL(
-              image ? image.publicURL : mainVisual,
-              siteUrl
-            )}`}
-            property="og:image"
-          />
-          <meta
-            content="あなたのオタクタイプ診断 by あにまーれ"
-            property="og:site_name"
-          />
-          <meta
-            content="あなたのオタクタイプを今すぐ診断"
-            property="og:title"
-          />
-          <meta
-            content={description || defaultDescription}
-            property="og:description"
-          />
-          <meta content="summary_large_image" name="twitter:card" />
-          <meta
-            content={`${new URL(
-              image ? image.publicURL : mainVisual,
-              siteUrl
-            )}`}
-            name="twitter:image"
-          />
-          <meta
-            content="あなたのオタクタイプ診断 by あにまーれ"
-            name="twitter:title"
-          />
-          <meta
-            content={description || defaultDescription}
-            name="twitter:description"
-          />
-        </Helmet>
-      )
-    }}
-  />
-)) as React.FunctionComponent<Props>
+  return (
+    <Helmet>
+      <link href={`${new URL(pathname, siteUrl)}`} rel="canonical" />
+      <meta content={description || defaultDescription} name="description" />
+      <meta content="website" property="og:type" />
+      <meta content={`${new URL(pathname, siteUrl)}`} property="og:url" />
+      <meta
+        content={`${new URL(image ? image.publicURL : mainVisual, siteUrl)}`}
+        property="og:image"
+      />
+      <meta
+        content="あなたのオタクタイプ診断 by あにまーれ"
+        property="og:site_name"
+      />
+      <meta content="あなたのオタクタイプを今すぐ診断" property="og:title" />
+      <meta
+        content={description || defaultDescription}
+        property="og:description"
+      />
+      <meta content="summary_large_image" name="twitter:card" />
+      <meta
+        content={`${new URL(image ? image.publicURL : mainVisual, siteUrl)}`}
+        name="twitter:image"
+      />
+      <meta
+        content="あなたのオタクタイプ診断 by あにまーれ"
+        name="twitter:title"
+      />
+      <meta
+        content={description || defaultDescription}
+        name="twitter:description"
+      />
+    </Helmet>
+  )
+}
+
+export default SEO
