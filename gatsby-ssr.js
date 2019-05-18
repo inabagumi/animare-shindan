690d5d5c36415cbb7f1ca78a946affbd232e5df6
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-05-19 04:06:14 +0900

    improve fonts handling

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,11 +3,36 @@ import { URL } from 'url'
 
 global.URL = URL
 
-export const onRenderBody = ({ setPostBodyComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+  setHeadComponents([
+    <link
+      as="style"
+      href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:700,900&amp;display=swap"
+      key="font-noto-sans-jp"
+      rel="preload"
+    />,
+    <link
+      as="style"
+      href="https://fonts.googleapis.com/css?family=Material+Icons"
+      key="font-material-icons"
+      rel="preload"
+    />
+  ])
+
   setPostBodyComponents([
     <script
       src="https://polyfill.io/v3/polyfill.min.js?features=URL"
       key="polyfills"
+    />,
+    <link
+      href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:700,900&amp;display=swap"
+      key="font-noto-sans-jp"
+      rel="stylesheet"
+    />,
+    <link
+      href="https://fonts.googleapis.com/css?family=Material+Icons"
+      key="font-material-icons"
+      rel="stylesheet"
     />
   ])
 }
