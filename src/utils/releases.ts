5e8b7cad0a679e59e6a68f240dd546c722dabea4
Yamagishi Kazutoshi <ykzts@haneru.dev>
2020-10-11 23:43:29 +0900

    feat(releases): use github api (#924)

--- src/utils/releases.ts
+++ src/utils/releases.ts
@@ -1,42 +1,24 @@
-import { compareDesc, formatISO, parseISO } from 'date-fns'
-import { promises as fs } from 'fs'
-import matter from 'gray-matter'
-import path from 'path'
+import { Octokit } from '@octokit/rest'
+
+const octokit = new Octokit()
 
 export type Release = {
   content: string
-  frontmatter: {
-    date: string
-    title: string
-  }
+  date: string
   slug: string
-}
-
-const releasesDirectory = path.join(process.cwd(), 'src', 'data', 'releases')
-
-export async function getRelease(slug: string): Promise<Release> {
-  const filePath = path.join(releasesDirectory, `${slug}.md`)
-  const rawContent = await fs.readFile(filePath, 'utf-8')
-  const { content, data } = matter(rawContent)
-
-  return {
-    content,
-    frontmatter: {
-      date: formatISO(data.date),
-      title: data.title as string
-    },
-    slug
-  }
+  title: string
 }
 
 export async function getAllReleases(): Promise<Release[]> {
-  const fileNames = await fs.readdir(releasesDirectory)
-  const slugs = fileNames.map((fileName) => fileName.replace(/\.md$/i, ''))
-  const releases = await Promise.all(slugs.map((slug) => getRelease(slug)))
+  const { data: releases } = await octokit.repos.listReleases({
+    owner: 'inabagumi',
+    repo: 'animare-shindan'
+  })
 
-  releases.sort((a, b) =>
-    compareDesc(parseISO(a.frontmatter.date), parseISO(b.frontmatter.date))
-  )
-
-  return releases
+  return releases.map<Release>((release) => ({
+    content: release.body,
+    date: release.created_at,
+    slug: release.tag_name,
+    title: release.name
+  }))
 }
