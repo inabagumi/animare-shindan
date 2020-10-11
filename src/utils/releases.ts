609f25369e9257dae548714004b41e451226adfd
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-10-12 07:32:47 +0900

    fix(releases): use github token

--- src/utils/releases.ts
+++ src/utils/releases.ts
@@ -1,6 +1,8 @@
 import { Octokit } from '@octokit/rest'
 
-const octokit = new Octokit()
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN
+})
 
 export type Release = {
   content: string
