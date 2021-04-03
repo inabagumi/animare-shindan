fab89daa920fa504ab1ebeed82da734578357532
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-04-03 12:14:50 +0900

    fix(deps): remove slash

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   pwa: {
     dest: '.next/static',
     disable: process.env.NODE_ENV === 'development',
-    sw: '/sw.js'
+    sw: 'sw.js'
   },
   rewrites() {
     return [
