237b6a756bfe67b35f2af426d9c2769dd5ba9de9
Yamagishi Kazutoshi <ykzts@haneru.dev>
2021-08-08 23:23:48 +0900

    fix(deps): fix types

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,9 +1,9 @@
-import remark from 'remark'
-import gfm from 'remark-gfm'
-import html from 'remark-html'
+import { remark } from 'remark'
+import remarkGfm from 'remark-gfm'
+import remarkHtml from 'remark-html'
 
 export async function convert(body: string): Promise<string> {
-  const file = await remark().use(gfm).use(html).process(body)
+  const file = await remark().use(remarkGfm).use(remarkHtml).process(body)
 
   return file.toString()
 }
