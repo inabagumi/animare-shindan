496f331a7dbff3fced8b75055e2a592d4557d65c
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-20 02:50:56 +0900

    build(prettier): remove comma

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,7 +2,7 @@ import inabagumiPrettierConfig from '@inabagumi/prettier-config'
 
 /** @type {import('prettier').Config} */
 const prettierConfig = {
-  ...inabagumiPrettierConfig,
+  ...inabagumiPrettierConfig
 }
 
 export default prettierConfig
