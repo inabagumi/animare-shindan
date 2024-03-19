1c23569b41d084d7e1814d7f12e192e0b239d311
Yamagishi Kazutoshi <ykzts@haneru.dev>
2024-03-19 23:20:09 +0900

    build(deps): remove unused plugins

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,8 +3,6 @@ import inabagumiPrettierConfig from '@inabagumi/prettier-config'
 /** @type {import('prettier').Config} */
 const prettierConfig = {
   ...inabagumiPrettierConfig,
-  plugins: ['prettier-plugin-tailwindcss'],
-  tailwindFunctions: ['clsx', 'twMerge']
 }
 
 export default prettierConfig
